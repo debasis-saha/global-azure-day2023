cdc3fa2a697720213064af9b55f0a38b5630fb9f
DEBASIS SAHA <saha.debasis@gmail.com>
2023-05-02 20:54:41 +0530

    Update wasm projects from json data..

---
+++ global-azure-day2023.Model.Shared/Model/Speaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace global_azure_day2023.Model.Shared.Models
+{
+    public class Speaker
+    {
+        public string SessionName { get; set; }
+
+        public string SpeakerName { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public string EventTime { get; set; }
+
+        public string SlotName { get; set; }
+    }
+}
