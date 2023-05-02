ea61837e8c048ccb9c81c6f5f29f0f8b5d401bc6
DEBASIS SAHA <saha.debasis@gmail.com>
2023-05-02 22:21:41 +0530

    Add Azure functions to fetch data..

--- global-azure-day2023/Program.cs
+++ global-azure-day2023/Program.cs
@@ -6,10 +6,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var baseAddress = builder.HostEnvironment.BaseAddress;
-//var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+//var baseAddress = builder.HostEnvironment.BaseAddress;
+var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+// configure httpclient
+// call the following code please add packageMicrosoft.Extensions.Http 3.1.0
+builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(baseAddress));
+// register the httpclient
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
+
+
 
 await builder.Build().RunAsync();
