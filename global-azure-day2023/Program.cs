157a687434d67cac430d8600538b55e93768e7d0
DEBASIS SAHA <saha.debasis@gmail.com>
2023-05-02 23:32:30 +0530

    update

--- global-azure-day2023/Program.cs
+++ global-azure-day2023/Program.cs
@@ -12,12 +12,4 @@ var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironmen
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
-// configure httpclient
-// call the following code please add packageMicrosoft.Extensions.Http 3.1.0
-builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(baseAddress));
-// register the httpclient
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
-
-
-
 await builder.Build().RunAsync();
