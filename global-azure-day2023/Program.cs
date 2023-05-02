d04e6fde5522fec210be9d67040c0d176a5d5cd4
DEBASIS SAHA <saha.debasis@gmail.com>
2023-05-02 07:36:43 +0530

    Add project files.

---
+++ global-azure-day2023/Program.cs
@@ -0,0 +1,15 @@
+using global_azure_day2023;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var baseAddress = builder.HostEnvironment.BaseAddress;
+//var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+await builder.Build().RunAsync();
